667b065c9a8aa066d1185bd8086f87c9176a2e7f
batsev <nikita.batsev@gmail.com>
2019-05-05 19:44:21 +0300

    initial commit

---
+++ dist/app.js
@@ -0,0 +1,49 @@
+// $(document).ready(function($) {
+//   $(".navbar ul li a").click(function() {
+//     $(".navbar ul li a").removeClass("active");
+//     $(this).addClass("active");
+//   });
+// });
+
+$(".navbar a").on("click", function(e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800
+    );
+  }
+});
+
+$(".navbar a").click(function() {
+  $(".navbar ul li a").removeClass("active");
+  $(this).addClass("active");
+});
+
+var parPosition = [];
+$(".par").each(function() {
+  parPosition.push($(this).offset().top);
+});
+
+$(document)
+  .scroll(function() {
+    var position = $(document).scrollTop(),
+      index;
+    for (var i = 0; i < parPosition.length; i++) {
+      if (position <= parPosition[i]) {
+        index = i;
+        break;
+      }
+    }
+    $(".navbar ul li a").removeClass("active");
+    $(".navbar ul li a:eq(" + index + ")").addClass("active");
+  })
+  .scroll();
+
+$(".navbar ul li a").click(function() {
+  $(".navbar ul li a").removeClass("active");
+  $(this).addClass("active");
+});
